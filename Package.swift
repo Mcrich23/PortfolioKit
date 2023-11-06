7fd3b50b04d5279e02fb49d536c908d56a9b5abb
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-11-05 22:52:38 -0800

    downgraded package.swift to 5.7

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,9 +20,13 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PortfolioKit"),
+            name: "PortfolioKit",
+            path: "Sources"
+        ),
         .testTarget(
             name: "PortfolioKitTests",
-            dependencies: ["PortfolioKit"]),
+            dependencies: ["PortfolioKit"],
+            path: "Tests"
+        ),
     ]
 )
